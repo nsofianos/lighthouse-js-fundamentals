01ea14dd52b287217e29c83c57b4240056b8c740
Nik Sofianos <nsofianos@hotmail.com>
2020-02-14 10:07:41 +0000

    concatenate 2 arrays into 1

---
+++ concat-array.js
@@ -0,0 +1,17 @@
+function concat(arr,arr2){
+  let concatArr = arr;
+  for (let i = 0; i < arr2.length; i++){
+    concatArr.push(arr2[i]);
+  }
+  return concatArr;
+}
+
+
+
+
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
